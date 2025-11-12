9318108880b7486cf093c148ed8003c6f11e713e
crocodileps <karouche.myriam@gmail.com>
2025-11-12 08:19:52 +0000

    save: avant ajout frontend Docker

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,16 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   async rewrites() {
     return [
       {
         source: '/api-backend/:path*',
-        destination: 'http://91.98.131.218:8000/:path*',
+        destination: 'http://91.98.131.218:8001/:path*',
       },
     ];
   },
 }
+
 module.exports = nextConfig
